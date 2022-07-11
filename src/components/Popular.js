104978fec592651cd6a7e012ecd65fc017c31206
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-07-11 05:22:43 +0300

    Add slider for popular recipes

--- src/components/Popular.js
+++ src/components/Popular.js
@@ -1,9 +1,51 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import { Splide, SplideSlide } from '@splidejs/react-splide';
+import '@splidejs/react-splide/css';
 
 function Popular() {
+  const [popular, setPopular] = useState([]);
+  useEffect(() => {
+    getPopular();
+  }, []);
+
+  const getPopular = async () => {
+    const key = process.env.REACT_APP_API_KEY;
+    const api = await fetch(
+      "https://api.spoonacular.com/recipes/random?apiKey=" + key + "&number=9"
+    );
+
+    const data = await api.json();
+
+    setPopular(data.recipes);
+  };
+
   return (
-    <div>Popular</div>
-  )
+    <div>
+      <h>Popular picks</h>
+      <Splide options={{
+        perPage : 4,
+        arrows : false,
+        pagination : false,
+        drag: 'free',
+        gap : '5 rem'
+        
+      }}>
+      {popular.map((recipe) => {
+        return (
+
+          <SplideSlide>
+          <div key={recipe.id}>
+            <p className="font-semibold text-black text-sm ">{recipe.title}</p>
+            <img src = {recipe.image} alt = {recipe.title} className ="rounded-3xl h-56 w-96 object-fill mr-8"/>
+          </div>
+          </SplideSlide>
+          
+        );
+      })}
+      </Splide>
+     
+    </div>
+  );
 }
 
 export default Popular;
