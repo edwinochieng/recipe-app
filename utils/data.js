bbc54c47e65d9e9ebc892ec47ad937428bc010a8
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-11-19 04:34:19 +0300

    sidebar data

---
+++ utils/data.js
@@ -0,0 +1,14 @@
+export const cuisines = [
+  { id: 0, name: "African", query: "african" },
+  { id: 1, name: "Chinese", query: "chinese" },
+  { id: 2, name: "French", query: "french" },
+  { id: 3, name: "Indian", query: "indian" },
+  { id: 4, name: "Italian", query: "italian" },
+  { id: 5, name: "Japanese", query: "japanese" },
+  { id: 6, name: "Korean", query: "korean" },
+  { id: 7, name: "Mediterranean", query: "mediterranean" },
+  { id: 8, name: "Mexican", query: "mexican" },
+  { id: 9, name: "Spanish", query: "spanish" },
+  { id: 10, name: "Thai", query: "thai" },
+  { id: 11, name: "Vietnamese", query: "vietnamese" },
+];
