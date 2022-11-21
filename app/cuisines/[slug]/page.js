0fe3a2ad9bb4925afc6006d4b7d00f64334895aa
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-11-22 01:19:58 +0300

    filter recipes by cuisine

--- app/cuisines/[slug]/page.js
+++ app/cuisines/[slug]/page.js
@@ -1,5 +1,30 @@
 import React from "react";
+import { baseUrl } from "../../../utils/baseUrl";
+import Recipe from "../../recipes/Recipe";
 
-export default function CuisinePage() {
-  return <div>CuisinePage</div>;
+const getCuisine = async (cuisine) => {
+  const res = await fetch(
+    `${baseUrl}/complexSearch?apiKey=${process.env.API_KEY}&&cuisine=${cuisine}&&number=12`,
+    { cache: "no-store" }
+  );
+
+  if (!res.ok) {
+    throw new Error("Data not fetched");
+  }
+  return res.json();
+};
+export default async function CuisinePage({ params }) {
+  const data = await getCuisine(params.slug);
+  return (
+    <div>
+      <h1 className="'text-gray-800 text-[18px] text-center font-semibold'">
+        {params.slug} cuisine
+      </h1>
+      <div className='py-4 flex flex-col md:flex-row justify-center items-center flex-wrap gap-6'>
+        {data.results.map((recipe) => (
+          <Recipe key={recipe.id} recipe={recipe} />
+        ))}
+      </div>
+    </div>
+  );
 }
