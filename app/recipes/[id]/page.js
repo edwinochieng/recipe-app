dbb1ffdc7061d894791249c7b33e5cec34b0d1c9
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-11-26 22:36:57 +0300

    recipe details page

--- app/recipes/[id]/page.js
+++ app/recipes/[id]/page.js
@@ -1,5 +1,43 @@
 import React from "react";
+import { baseUrl } from "../../../utils/baseUrl";
+import Details from "./Details";
+import SimilarRecipes from "./SimilarRecipes";
 
-export default function RecipeDetails() {
-  return <div>RecipeDetails</div>;
+const getRecipeDetails = async (id) => {
+  const res = await fetch(`${baseUrl}/${id}/information`, {
+    cache: "no-store",
+  });
+
+  if (!res.ok) {
+    throw new Error("Data not fetched");
+  }
+
+  return res.json();
+};
+
+const getSimilarRecipes = async (id) => {
+  const res = await fetch(`${baseUrl}/${id}/similar`, { cache: "no-store" });
+
+  if (!res.ok) {
+    throw new Error("Data not fetched");
+  }
+
+  return res.json();
+};
+
+export default async function RecipeDetails({ searchParams }) {
+  const recipeData = await getRecipeDetails(searchParams.id);
+  const similarData = await getSimilarRecipes(searchParams.id);
+
+  const [details, recipes] = await Promise.all([recipeData, similarData]);
+  return (
+    <div>
+      <div>
+        <Details data={details} />
+      </div>
+      <div>
+        <SimilarRecipes data={recipes} />
+      </div>
+    </div>
+  );
 }
