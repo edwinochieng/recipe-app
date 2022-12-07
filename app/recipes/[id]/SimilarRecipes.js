ad7b9292d2fd70ba245cab867a32185b93fd45d4
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-07 21:32:11 +0300

    similar recipes list

--- app/recipes/[id]/SimilarRecipes.js
+++ app/recipes/[id]/SimilarRecipes.js
@@ -1,5 +1,22 @@
+"use client";
+
 import React from "react";
 
-export default function SimilarRecipes() {
-  return <div>SimilarRecipes</div>;
+import Link from "next/link";
+
+export default function SimilarRecipes({ data }) {
+  return (
+    <div className='pt-2 pb-4 max-w-[700px] w-full'>
+      <h1 className='pb-1 font-semibold text-[16px] text-gray-700'>
+        Similar Recipes
+      </h1>
+      <ul className='pl-6 list-disc text-[15px] font-medium text-gray-600 '>
+        {data.map((recipe) => (
+          <li key={recipe.title} className='hover:text-gray-400'>
+            <Link href={`/recipes/${recipe.id}`}>{recipe.title}</Link>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
