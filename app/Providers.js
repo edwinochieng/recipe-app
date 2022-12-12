331f8dac67f1fcbd05ccfa5518e8df611c1b28e0
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-12 19:52:49 +0300

    remove react-query

--- app/Providers.js
+++ app/Providers.js
@@ -1,13 +1,8 @@
-import React, { useContext } from "react";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+"use client";
+
+import React from "react";
 import FavouritesProvider from "./context";
 
-const queryClient = new QueryClient();
-
 export default function Providers({ children }) {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <FavouritesProvider>{children}</FavouritesProvider>
-    </QueryClientProvider>
-  );
+  return <FavouritesProvider>{children}</FavouritesProvider>;
 }
