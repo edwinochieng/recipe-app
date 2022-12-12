7a3ff883c3124322b610531d721e24bdd9dcbd1f
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-12 19:55:45 +0300

    increase recipes fetched to 99 pass down recipe data to Search component limit recipes on homepage to 9

--- app/page.jsx
+++ app/page.jsx
@@ -4,7 +4,7 @@ import { baseUrl } from "../utils/baseUrl";
 
 const getPopularRecipes = async () => {
   const res = await fetch(
-    `${baseUrl}/random?apiKey=${process.env.API_KEY}&&number=9`
+    `${baseUrl}/random?apiKey=${process.env.API_KEY}&&number=90`
   );
 
   if (!res.ok) {
@@ -20,13 +20,13 @@ export default async function Home() {
   //console.log(data.recipes);
   return (
     <div className='w-full mx-auto'>
-      <Search />
+      <Search recipes={data.recipes} />
       <div className='mt-6 sm:mt-8'>
         <h1 className='text-gray-800 text-[18px] text-center font-semibold'>
           Popular Picks
         </h1>
         <div className='pt-2 pb-6 flex flex-col md:flex-row justify-center items-center flex-wrap gap-x-6'>
-          {data.recipes.map((recipe) => (
+          {data.recipes.slice(0, 9).map((recipe) => (
             <Recipe key={recipe.id} recipe={recipe} />
           ))}
         </div>
