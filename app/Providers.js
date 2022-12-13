d137df24ef0d7f64929649f4260822fa596b2ade
Edwin Ochieng <ochiengedwin440@gmail.com>
2022-12-13 21:44:48 +0300

    add react-toastify

--- app/Providers.js
+++ app/Providers.js
@@ -2,7 +2,14 @@
 
 import React from "react";
 import FavouritesProvider from "./context";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
 export default function Providers({ children }) {
-  return <FavouritesProvider>{children}</FavouritesProvider>;
+  return (
+    <FavouritesProvider>
+      <ToastContainer position='bottom-center' autoClose={2500} />
+      {children}
+    </FavouritesProvider>
+  );
 }
