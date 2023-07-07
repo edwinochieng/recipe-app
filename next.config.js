b9e9d3bb6f296d5c5eac58eb29f8c4bf7fde9236
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-07 22:17:43 +0300

    remove experimental features

--- next.config.js
+++ next.config.js
@@ -1,17 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-};
 
 module.exports = {
-  experimental: {
-    fontLoaders: [
-      { loader: "@next/font/google", options: { subsets: ["Poppins"] } },
-    ],
-    appDir: true,
-  },
   images: {
     domains: ["spoonacular.com"],
   },
