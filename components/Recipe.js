8a85afcc0a144bb1e55a1310efa6eaa01bd8a6b6
Edwin Ochieng <ochiengedwin440@gmail.com>
2023-07-08 19:22:22 +0300

    change save recipe icons

--- components/Recipe.js
+++ components/Recipe.js
@@ -1,11 +1,30 @@
 "use client";
 
-import React, { useContext } from "react";
+import React from "react";
 import Image from "next/image";
 import Link from "next/link";
-import { BsBookmarkStarFill } from "react-icons/bs";
+import { MdFavoriteBorder, MdFavorite } from "react-icons/md";
+import { useFavoriteRecipesStore } from "../store/store";
 
 export default function Recipe({ recipe }) {
+  const savedRecipes = useFavoriteRecipesStore(
+    (state) => state.favoriteRecipes
+  );
+  const addToFavorites = useFavoriteRecipesStore((state) => state.addRecipe);
+  const removeFromFavorites = useFavoriteRecipesStore(
+    (state) => state.removeFromFavorites
+  );
+
+  const isRecipeSaved = savedRecipes.some((item) => item.id === recipe.id);
+
+  const handleSaveRecipe = () => {
+    if (isRecipeSaved) {
+      removeFromFavorites(recipe.id);
+    } else {
+      addToFavorites(recipe);
+    }
+  };
+
   return (
     <div className='my-2 relative max-w-[300px] max-h-[200px] rounded-2xl shadow-xl transform hover:scale-110 ease-in duration-200'>
       <Link href={`/recipes/${recipe.id}`}>
@@ -21,7 +40,13 @@ export default function Recipe({ recipe }) {
         </p>
       </Link>
       <div className='absolute top-2 right-2 z-5 cursor-pointer p-1.5 bg-gray-50 shadow-lg rounded-lg text-gray-900'>
-        <BsBookmarkStarFill size={24} />
+        <div onClick={handleSaveRecipe}>
+          {isRecipeSaved ? (
+            <MdFavorite size={24} className='text-red-600' />
+          ) : (
+            <MdFavoriteBorder size={24} />
+          )}
+        </div>
       </div>
     </div>
   );
